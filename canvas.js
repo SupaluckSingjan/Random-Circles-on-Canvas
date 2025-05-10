9d5e60296c98ce06a404a6223e7307d1dd093bbd
Supaluck Singjan <supalucksingjan@gmail.com>
2025-05-10 21:13:03 +0700

    Add Boundaries & Velocity

--- canvas.js
+++ canvas.js
@@ -11,18 +11,39 @@ let dx = (Math.random() - 0.5) *8;
 let dy = (Math.random() - 0.5) *8;
 let radius = 50;
 
-const Circle = (x,y) => {
+function Circle(x,y) {
     this.x = x;
     this.y = y;
 
-    this.darw = function() {
-        
+    // Circle
+    this.draw = function() {
+        c.beginPath();
+        c.arc(x, y, radius, Math.PI*2, false);
+        c.strokeStyle = "#c55a83";
+        c.stroke();
     }
+
+    // Boundaries & velocity
+    this.update = function() {
+            if( x + radius > innerWidth || x  < 0 + radius) {
+            dx = -dx; 
+        }
+
+        if( y + radius > innerHeight || y  < 0 + radius) {
+            dy = -dy; 
+        }
+
+        x += dx;
+        y += dy; 
+        }
 }
+
+let circle = new Circle(200,200);
 
 const animate = () => {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth,innerHeight);
+    circle.draw();
 
    // Circle
     c.beginPath();
