cd145ad1f45f266eddab9bc0fdb5f2e7e53442c2
Supaluck Singjan <supalucksingjan@gmail.com>
2025-05-11 01:33:55 +0700

    Add many circles

--- canvas.js
+++ canvas.js
@@ -5,62 +5,69 @@ canvas.height = window.innerHeight;
 
 let c = canvas.getContext('2d');
 
-let x = Math.random() * innerWidth; 
-let y = Math.random() * innerHeight;
-let dx = (Math.random() - 0.5) *8;
-let dy = (Math.random() - 0.5) *8;
-let radius = 50;
 
-function Circle(x,y) {
+
+function Circle(x,y, dx, dy, radius) {
     this.x = x;
     this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
 
     // Circle
     this.draw = function() {
         c.beginPath();
-        c.arc(x, y, radius, Math.PI*2, false);
+        c.arc(this.x, this.y, this.radius, Math.PI*2, false);
         c.strokeStyle = "#c55a83";
         c.stroke();
     }
 
-    // Boundaries & velocity
+    // Boundaries & Velocity
     this.update = function() {
-            if( x + radius > innerWidth || x  < 0 + radius) {
-            dx = -dx; 
+        if( this.x + this.radius > innerWidth || this.x  < 0 + this.radius) {
+            this.dx = -this.dx; 
         }
 
-        if( y + radius > innerHeight || y  < 0 + radius) {
-            dy = -dy; 
+        if( this.y + this.radius > innerHeight || this.y  < 0 + this.radius) {
+            this.dy = -this.dy; 
         }
 
-        x += dx;
-        y += dy; 
+        this.x += this.dx;
+        this.y += this.dy; 
+
+        this.draw();
         }
+
+        
 }
 
-let circle = new Circle(200,200);
+/* let x = Math.random() * innerWidth; 
+let y = Math.random() * innerHeight;
+let dx = (Math.random() - 0.5) *8;
+let dy = (Math.random() - 0.5) *8;
+let radius = 50; */
+
+let circleArray = [];
+
+for(let i =0;i < 100; i++) {
+    let x = Math.random() * innerWidth; 
+    let y = Math.random() * innerHeight;
+    let dx = (Math.random() - 0.5) *8;
+    let dy = (Math.random() - 0.5) *8;
+    let radius = 50;
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+
+console.log(circleArray)
+
 
 const animate = () => {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth,innerHeight);
-    circle.draw();
-
-   // Circle
-    c.beginPath();
-    c.arc(x, y, radius, Math.PI*2, false);
-    c.strokeStyle = "#c55a83";
-    c.stroke();
-
-    if( x + radius > innerWidth || x  < 0 + radius) {
-        dx = -dx; 
+    
+    for(let i = 0; i <circleArray.length; i++) {
+        circleArray[i].update();
     }
-
-     if( y + radius > innerHeight || y  < 0 + radius) {
-        dy = -dy; 
-    }
-
-    x += dx;
-    y += dy; 
     
 }
 
