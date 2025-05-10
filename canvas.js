099ec8ca500dc716f72a6f1330c498b2033d7e8e
Supaluck Singjan <supalucksingjan@gmail.com>
2025-05-10 15:53:26 +0700

    Add requestAnimationFrame(animate)

--- canvas.js
+++ canvas.js
@@ -1,11 +1,25 @@
-let canvas = document.querySelector('canvas');
+let canvas = document.getElementById("myCanvas");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let c = canvas.getContext('2d');
 
-c.beginPath();
-c.arc(250, 250, 50, Math.PI*2, false);
-c.strokeStyle = "#c55a83";
-c.stroke();
+let x = 250;
+let y = 250;
+let radius = 50;
+
+const animate = () => {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth,innerHeight);
+
+   // Circle
+    c.beginPath();
+    c.arc(x, y, radius, Math.PI*2, false);
+    c.strokeStyle = "#c55a83";
+    c.stroke();
+
+    x += 10
+}
+
+animate();
